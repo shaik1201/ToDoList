bb85a12cd2b73576f86117bb0dc67d96ce13c3b7
shaik <shaikiko12@gmail.com>
2021-08-09 20:12:07 +0300

    added styles

--- todo.js
+++ todo.js
@@ -14,11 +14,24 @@ document.addEventListener('DOMContentLoaded', function(){
        const task = document.querySelector('#task').value;
        const li = document.createElement('li');
        li.innerHTML = task;
+       
        document.querySelector('#tasks').append(li);
        document.querySelector('#task').value = '';
        document.querySelector('#submit').disabled = true;
+       
+       // bug: the task gets deleted by clicking on the screen and not on the task itself.
+       document.addEventListener('click', function(){
+           li.style.textDecoration = 'line-through'
+       })
+       document.addEventListener('dblclick', function(){
+        document.querySelector('#tasks').removeChild(li);
+    })
        return false;
+        }
     }
+    
 
     
-});
+);
+
+
